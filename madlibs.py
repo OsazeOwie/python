c022c682d0f2d3afd1ca206e1540bb56087e3205
oz <osazeowie0@gmail.com>
2022-10-12 00:43:28 +0100

    add madlib game

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+print("mad libs game")
+print("fill each below to go to next question after you typed the first one press enter to go to the scond one ")
+
+
+name = input("Enter a name:  ")
+age = input("Enter a age:  ")
+celebrity = input("Enter a celebrity:  ")
+
+print("There once was a man named " + name)
+print("He was "+ age)
+print("He Love "+celebrity)
